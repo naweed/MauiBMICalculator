80c853f0417d4f749a283b88854be834ac95600a
Naweed Akram <naweed@xgeno.com>
2022-08-21 15:40:35 +0300

    Commit

---
+++ src/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using System.Globalization;
+global using SkiaSharp;
+global using SkiaSharp.Views.Maui.Controls.Hosting;
+global using CommunityToolkit.Mvvm.Input;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using MauiBMICalculator.Views;
+global using MauiBMICalculator.ViewModels;
